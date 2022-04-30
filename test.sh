6236d8ff49242211e579b15e5373e22744e176bf
AshtonGross1 <102486746+AshtonGross1@users.noreply.github.com>
2022-04-30 15:38:26 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+array=()
+while IFS= read -r line; do
+   array+=("$line")
+done <lines.txt
+
+for i in ${array[@]}; do 
+    printf $i >> test.log;
+    printf date +%s >> test.log;
+done
+
+git commit -m "test.sh" test.sh
+
+git push -u origin master
